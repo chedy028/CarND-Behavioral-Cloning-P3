948b657b07a60e29b0082a93f232622dcab79f44
Norman Chen <chedy028@gmail.com>
2018-03-11 20:26:41 -0500

    tweak the model

--- model.py
+++ model.py
@@ -1,8 +1,22 @@
 import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, Flatten
+from utility import batch_generator
 import csv
 import cv2
 import numpy as np
-import utility
+'''
+data_dir = './data'
+data_df = pd.read_csv('./data/driving_log.csv')
+
+X = data_df[['center', 'left', 'right']].values
+y = data_df['steering'].values
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=0)'''
+
+
 # Load the csv file to get the figure name
 lines = []
 with open ('./data/driving_log.csv') as csvfile:
@@ -24,31 +38,29 @@ for line in lines:
         measurement = float(line[3])
         measurements.append(measurement)
 
+X = np.array(images)
+y = np.array(measurements)
 
-X_raw = np.array(images)
-y_raw = np.array(measurements)
-
-from sklearn.model_selection import train_test_split
-X_train, X_valid, y_train, y_valid = train_test_split(X_raw, y_raw, test_size = 0.2, random_state=0)
-
-
-
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
-from keras.layers import Convolution2D
-from keras.layers.pooling import MaxPooling2D
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=0)
 
 model = Sequential()
-model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
-model.add(Convolution2D(24,5,5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(36,5,5, activation='relu'))
-model.add(MaxPooling2D())
+model.add(Conv2D(16, 3, 3, input_shape=(32, 128, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, 3, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, 3, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(.5))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(.25))
+model.add(Dense(20, activation='relu'))
 model.add(Dense(1))
+model.compile(optimizer=Adam(lr=1e-04), loss='mean_squared_error')
+model.summary()
 
-model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
-model.save('model.h5')
+model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.004))
+
+generator = batch_generator(X_train, y_train, 10)
+model.fit_generator(generator, samples_per_epoch=50, nb_epoch=10)
--- utility.py
+++ utility.py
@@ -6,6 +6,10 @@ from sklearn.model_selection import train_test_split
 
 
 IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 66, 200, 3
+INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+
+def load_image(img_dir, img_file):
+    return mpimg.imread(os.path.join(img_dir, img_file.strip()))
 
 def crop(image):
     '''removing the sky at the top and the car front at the bottom'''
@@ -28,3 +32,55 @@ def preprocess(image):
     image = resize(image)
     image = rgb2yuv(image)
     return image
+
+def adjust_image(img_dir, center, left, right, steering_angle):
+    '''adjust the stering angle'''
+    choise = np.random.choise(3)
+    if choise == 0:
+        return load_image(img_dir, left), steering_angle + 0.2
+    elif choise == 1:
+        return load_image(img_Dir, right), steering_angle - 0.2
+    return load_image(img_dir, center), steering_angle
+
+def flip_img(image, steering_angle):
+    '''randomly flip the image'''
+    if np.random.rand()<0.5:
+        image = cv2.flip(image, 1)
+        steering_angle = -steering_angle
+    return image, steering_angle
+
+def choose_image(data_dir, center, left, right, steering_angle):
+    """
+    Randomly choose an image from the center, left or right, and adjust
+    the steering angle.
+    """
+    choice = np.random.choice(3)
+    if choice == 0:
+        return load_image(data_dir, left), steering_angle + 0.2
+    elif choice == 1:
+        return load_image(data_dir, right), steering_angle - 0.2
+    return load_image(data_dir, center), steering_angle
+
+
+
+def augument(center, left, right, steering_angle, range_x=100, range_y=10):
+    """
+    Generate an augumented image and adjust steering angle.
+    (The steering angle is associated with the center image)
+    """
+    image, steering_angle = choose_image(center, left, right, steering_angle)
+    image, steering_angle = flip_img(image, steering_angle)
+
+    return image, steering_angle
+
+
+def batch_generator(features, labels, batch_size):
+    batch_features = np.zeros((batch_size, 66, 200, 3))
+    labels = np.zeros((batch_size, 1))
+
+    while True:
+        for i in range(batch_size):
+            index = np.random.choice(len(features), 1)
+            batch_features[i] = features[index]
+            batch_labels[i] = labels[index]
+        yield batch_features, batch_labels
