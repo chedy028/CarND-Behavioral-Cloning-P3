81dbee401d64f9f90c9daa84b916e8785a22658a
Norman Chen <chedy028@gmail.com>
2018-03-18 14:37:20 -0500

    model.py working

--- model.py
+++ model.py
@@ -1,107 +1,157 @@
-import pandas as pd
-import os
-from sklearn.model_selection import train_test_split
-from keras.models import Sequential
-from keras.optimizers import Adam
-from keras.layers import Lambda, Convolution2D, MaxPooling2D, Dropout, Dense, Flatten,Cropping2D
-from utility import augment
-import csv
 import cv2
+import csv
 import numpy as np
+import os
+
+def getLinesFromDrivingLogs(dataPath, skipHeader=False):
+    """
+    Returns the lines from a driving log with base directory `dataPath`.
+    If the file include headers, pass `skipHeader=True`.
+    """
+    lines = []
+    with open(dataPath + '/driving_log.csv') as csvFile:
+        reader = csv.reader(csvFile)
+        if skipHeader:
+            next(reader, None)
+        for line in reader:
+            lines.append(line)
+    del (lines[0])
+    return lines
 
 
-'''data_dir = './data'
-data_df = pd.read_csv('./data/driving_log.csv')
-X = data_df[['center', 'left', 'right']].values
-y = data_df['steering'].values'''
+def findImages(dataPath):
+    """
+    Finds all the images needed for training on the path `dataPath`.
+    Returns `([centerPaths], [leftPath], [rightPath], [measurement])`
+    """
+    directories = [x[0] for x in os.walk(dataPath)]
+    dataDirectories = list(filter(lambda directory: os.path.isfile(directory + '/driving_log.csv'), directories))
+    centerTotal = []
+    leftTotal = []
+    rightTotal = []
+    measurementTotal = []
+    for directory in dataDirectories:
+        lines = getLinesFromDrivingLogs(directory)
+        center = []
+        left = []
+        right = []
+        measurements = []
+        for line in lines:
+            measurements.append(float(line[3]))
+            center.append(directory + '/' + line[0].strip())
+            left.append(directory + '/' + line[1].strip())
+            right.append(directory + '/' + line[2].strip())
+        centerTotal.extend(center)
+        leftTotal.extend(left)
+        rightTotal.extend(right)
+        measurementTotal.extend(measurements)
 
-samples = []
-with open('./data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        samples.append(line)
+    return (centerTotal, leftTotal, rightTotal, measurementTotal)
 
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+def combineImages(center, left, right, measurement, correction):
+    """
+    Combine the image paths from `center`, `left` and `right` using the correction factor `correction`
+    Returns ([imagePaths], [measurements])
+    """
+    imagePaths = []
+    imagePaths.extend(center)
+    imagePaths.extend(left)
+    imagePaths.extend(right)
+    measurements = []
+    measurements.extend(measurement)
+    measurements.extend([x + correction for x in measurement])
+    measurements.extend([x - correction for x in measurement])
+    return (imagePaths, measurements)
+
+import sklearn
 
 def generator(samples, batch_size=32):
+    """
+    Generate the required images and measurments for training/
+    `samples` is a list of pairs (`imagePath`, `measurement`).
+    """
     num_samples = len(samples)
-    while True: # Loop forever so the generator never terminates
-        sklearn.utils.shuffle(samples)
+    while 1: # Loop forever so the generator never terminates
+        samples = sklearn.utils.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
+
             images = []
             angles = []
-            for batch_sample in batch_samples:
-                center = './data/IMG/'+batch_sample[0].split('/')[-1]
-                left = './data/IMG/'+batch_sample[1].split('/')[-1]
-                right = './data/IMG/'+batch_sample[1].split('/')[-1]
-                image_center = cv2.imread(center)
-                image_left = cv2.imread(left)
-                image_right = cv2.imread(right)
-                angle = float(batch_sample[3])
-                images.append([image_center, image_left, image_right])
-                angles.append(angle)
-
+            for imagePath, measurement in batch_samples:
+                originalImage = cv2.imread(imagePath)
+                image = cv2.cvtColor(originalImage, cv2.COLOR_BGR2RGB)
+                images.append(image)
+                angles.append(measurement)
+                # Flipping
+                images.append(cv2.flip(image,1))
+                angles.append(measurement*-1.0)
 
             # trim image to only see section with road
+            inputs = np.array(images)
+            outputs = np.array(angles)
+            yield sklearn.utils.shuffle(inputs, outputs)
 
-            X_train = np.array(images)
-            y_train = np.array(angles)
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D
+from keras.layers.pooling import MaxPooling2D
+import matplotlib.pyplot as plt
 
-            yield sklearn.utils.shuffle(X_train, y_train)
+def createPreProcessingLayers():
+    """
+    Creates a model with the initial pre-processing layers.
+    """
+    model = Sequential()
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((50,20), (0,0))))
+    return model
+
+def nVidiaModel():
+    """
+    Creates nVidea Autonomous Car Group model
+    """
+    model = createPreProcessingLayers()
+    model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    return model
+
+
+# Reading images locations.
+centerPaths, leftPaths, rightPaths, measurements = findImages('data')
+imagePaths, measurements = combineImages(centerPaths, leftPaths, rightPaths, measurements, 0.2)
+print('Total Images: {}'.format( len(imagePaths)))
+
+# Splitting samples and creating generators.
+from sklearn.model_selection import train_test_split
+samples = list(zip(imagePaths, measurements))
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+print('Train samples: {}'.format(len(train_samples)))
+print('Validation samples: {}'.format(len(validation_samples)))
 
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
-'''
 
+# Model creation
+model = nVidiaModel()
 
-# Load the csv file to get the figure name
-lines = []
-with open ('./data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
+# Compiling and training the model
+model.compile(loss='mse', optimizer='adam')
+history_object = model.fit_generator(train_generator, samples_per_epoch= \
+                 len(train_samples), validation_data=validation_generator, \
+                 nb_val_samples=len(validation_samples), nb_epoch=3, verbose=1)
 
-del(lines[0])#delete the header
-
-images = []
-measurements = []
-for line in lines:
-    for i in range(3):
-        source_path = line[i] #read the middle, left, right images
-        filename = source_path.split('/')[-1]
-        current_path = './data/IMG/' + filename # for running code in AWS
-        image = cv2.imread(current_path)
-        images.append(image)
-        measurement = float(line[3])
-        measurements.append(measurement)
-
-X = np.array(images)
-y = np.array(measurements)
-
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=0)'''
-def my_resize_function(input):
-    from keras.backend import tf as ktf
-    return ktf.image.resize_images(input, (18, 60)
-
-
-
-model = Sequential
-model.add(Cropping2D(cropping=((70, 25), (1, 1)), input_shape=(160, 320, 3)))
-model.add(Lambda(lambda x: my_resize_function(x)))
-model.add(Lambda(lambda x: x/127.5-1.0, input_shape=(18,60,3)))
-model.add(Convolution2D(24, 5, 5, activation='relu', subsample=(2, 2)))
-model.add(Convolution2D(36, 5, 5, activation='relu', subsample=(2, 2)))
-model.add(Convolution2D(48, 5, 5, activation='relu', subsample=(2, 2)))
-model.add(Convolution2D(64, 3, 3, activation='relu'))
-model.add(Convolution2D(64, 3, 3, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Flatten())
-model.add(Dense(100, activation='relu'))
-model.add(Dense(50, activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(1))
-model.summary()
-
-model.compile(loss='mean_squared_error', optimizer='Adam')
-model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+model.save('model.h5')
+print(history_object.history.keys())
+print('Loss')
+print(history_object.history['loss'])
+print('Validation Loss')
+print(history_object.history['val_loss'])
