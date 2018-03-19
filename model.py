bb0a0c0bca589736410d1868a801a158000c0552
Norman Chen <chedy028@gmail.com>
2018-03-18 20:00:59 -0500

    edit the model

--- model.py
+++ model.py
@@ -1,69 +1,9 @@
-import cv2
-import csv
-import numpy as np
-import os
 
-def getLinesFromDrivingLogs(dataPath, skipHeader=False):
-    """
-    Returns the lines from a driving log with base directory `dataPath`.
-    If the file include headers, pass `skipHeader=True`.
-    """
-    lines = []
-    with open(dataPath + '/driving_log.csv') as csvFile:
-        reader = csv.reader(csvFile)
-        if skipHeader:
-            next(reader, None)
-        for line in reader:
-            lines.append(line)
-    del (lines[0])
-    return lines
-
-
-def findImages(dataPath):
-    """
-    Finds all the images needed for training on the path `dataPath`.
-    Returns `([centerPaths], [leftPath], [rightPath], [measurement])`
-    """
-    directories = [x[0] for x in os.walk(dataPath)]
-    dataDirectories = list(filter(lambda directory: os.path.isfile(directory + '/driving_log.csv'), directories))
-    centerTotal = []
-    leftTotal = []
-    rightTotal = []
-    measurementTotal = []
-    for directory in dataDirectories:
-        lines = getLinesFromDrivingLogs(directory)
-        center = []
-        left = []
-        right = []
-        measurements = []
-        for line in lines:
-            measurements.append(float(line[3]))
-            center.append(directory + '/' + line[0].strip())
-            left.append(directory + '/' + line[1].strip())
-            right.append(directory + '/' + line[2].strip())
-        centerTotal.extend(center)
-        leftTotal.extend(left)
-        rightTotal.extend(right)
-        measurementTotal.extend(measurements)
-
-    return (centerTotal, leftTotal, rightTotal, measurementTotal)
-
-def combineImages(center, left, right, measurement, correction):
-    """
-    Combine the image paths from `center`, `left` and `right` using the correction factor `correction`
-    Returns ([imagePaths], [measurements])
-    """
-    imagePaths = []
-    imagePaths.extend(center)
-    imagePaths.extend(left)
-    imagePaths.extend(right)
-    measurements = []
-    measurements.extend(measurement)
-    measurements.extend([x + correction for x in measurement])
-    measurements.extend([x - correction for x in measurement])
-    return (imagePaths, measurements)
-
-import sklearn
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D
+from keras.layers.pooling import MaxPooling2D
+import matplotlib.pyplot as plt
+from utility import getimage, combineImages
 
 def generator(samples, batch_size=32):
     """
@@ -92,10 +32,7 @@ def generator(samples, batch_size=32):
             outputs = np.array(angles)
             yield sklearn.utils.shuffle(inputs, outputs)
 
-from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D
-from keras.layers.pooling import MaxPooling2D
-import matplotlib.pyplot as plt
+
 
 def createPreProcessingLayers():
     """
@@ -125,7 +62,7 @@ def nVidiaModel():
 
 
 # Reading images locations.
-centerPaths, leftPaths, rightPaths, measurements = findImages('data')
+centerCamera, leftCamera, rightCamera, measurements = getimage('data')
 imagePaths, measurements = combineImages(centerPaths, leftPaths, rightPaths, measurements, 0.2)
 print('Total Images: {}'.format( len(imagePaths)))
 
--- utility.py
+++ utility.py
@@ -1,24 +1,62 @@
-import cv2, os
+import cv2
+import csv
 import numpy as np
-import matplotlib.image as mpimg
-import pandas as pd
-from sklearn.model_selection import train_test_split
-INPUT_SHAPE= 66, 200
+import os
+import sklearn
 
-def flipping(images, angles):
-    aug_images, aug_angles = [], []
-    for image, angle in zip(images, angles):
-        aug_images.append(image)
-        aug_angles.append(angle)
-        # Flipping Images (horizontally) and inverting Steering Angles
-        aug_images.append(np.fliplr(image))
-        aug_angles.append(angle * -1.0)
-    return aug_images, aug_angles
+def getdata(path):
+    """
+    get list of data from driving_log csv
+    """
+    lines = []
+    with open(path + '/driving_log.csv') as csvFile:
+        reader = csv.reader(csvFile)
+        for line in reader:
+            lines.append(line)
+    del (lines[0]) #delete the headers
+    return lines
 
-def resize(images):
-    '''Resize the image to the input shape used by the CNN'''
-    return cv2.resize(images, INPUT_SHAPE)
-def augment(images, angles):
-    flipping(images, angles)
-    resize(images)
-    return images,angles
+
+def getimage(path):
+    """
+    Finds all the images needed for training on the path `dataPath`.
+    Returns `([centerPaths], [leftPath], [rightPath], [measurement])`
+    """
+    directories = [x[0] for x in os.walk(path)]
+    data = list(filter(lambda directory: os.path.isfile(directory + '/driving_log.csv'), directories))
+    centerCamera = []
+    leftCamera = []
+    rightCamera = []
+    measurement = []
+    for directory in data:
+        lines = getdata(directory)
+        center = []
+        left = []
+        right = []
+        measurements = []
+        for line in lines:
+            measurements.append(float(line[3]))
+            center.append(directory + '/' + line[0].strip())
+            left.append(directory + '/' + line[1].strip())
+            right.append(directory + '/' + line[2].strip())
+        centerCamera.extend(center)
+        leftCamera.extend(left)
+        rightCamera.extend(right)
+        measurement.extend(measurements)
+
+    return centerCamera, leftCamera, rightCamera, measurement
+
+def combineImages(center, left, right, measurement, correction):
+    """
+    Combine the image paths from `center`, `left` and `right` using the correction factor `correction`
+    Returns ([imagePaths], [measurements])
+    """
+    image = []
+    image.extend(center)
+    image.extend(left)
+    image.extend(right)
+    measurements = []
+    measurements.extend(measurement)
+    measurements.extend([x + correction for x in measurement])
+    measurements.extend([x - correction for x in measurement])
+    return (image, measurements)
